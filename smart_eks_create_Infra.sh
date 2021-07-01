7b38fdf0731a3ad31ae7a7ed6f01fcfaa99dc21a
Ganesh Pise <piseganesh123@gmail.com>
2021-07-01 14:50:02 +0530

    Updated infra creation scripts for fargate

--- smart_eks_create_Infra.sh
+++ smart_eks_create_Infra.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 # Ganesh.pise - 30 June 2021
-# script to create EKS cluster in Mumbai region with smalles possible server configuration
+# script to create EKS cluster in Mumbai region with smalles possible server configuration for node group
 
 eksctl create cluster \
---name smart-eks-cluster \
+--name smart-eks-ng-cluster \
 --region ap-south-1 \
 --nodegroup-name smart-nd-grp \
 --node-type t3.small \
 --nodes 2 \
 --managed
+
+# script to create EKS cluster in Mumbai region with fargate
+
+eksctl create cluster \
+--name smart-eks-farg-cluster \
+--region ap-south-1 \
+--fargate
